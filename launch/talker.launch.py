e6945d60c2ada517e57e3acac30f01c5e491359f
Luyolo <luyitumeleng@gmail.com>
2023-10-13 05:57:23 +0200

    first commit

---
+++ launch/talker.launch.py
@@ -0,0 +1,10 @@
+rom launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='demo_nodes_cpp',
+            executable='talker'
+        )
+    ])
